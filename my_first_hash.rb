738f02f27b0ee78f87071759f02d91555d478b02
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-14 09:07:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,9 +21,9 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
 shipping_manifest["oil paintings"]
+end
+
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5,
